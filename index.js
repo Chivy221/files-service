d9331adeeaf4592349b5b49e6941ba75f18382b6
Chivy221 <danilius227@gmail.com>
2025-06-08 16:29:39 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,17 +1,24 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const app = express();
-require('dotenv').config();
+const dotenv = require('dotenv');
+const fileRoutes = require('./routes/files');
+const { sendLog } = require('./utils/logger');
 
-const filesRoutes = require('./routes/files');
+dotenv.config();
+const app = express();
 app.use(express.json());
-app.use('/files', filesRoutes);
-app.get('/ping', (_, res) => res.send('pong'));
+
+app.use('/files', fileRoutes);
+
+app.get('/health', (, res) => res.json({ status: 'ok' }));
+app.get('/metrics', (, res) => res.send('files_service_total_requests 42'));
 
 mongoose.connect(process.env.MONGO_URL, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
+useNewUrlParser: true,
+useUnifiedTopology: true,
 }).then(() => {
-  console.log('File DB connected');
-  app.listen(3000, () => console.log('File service on port 3000'));
-}).catch(err => console.error(err));
+app.listen(process.env.PORT, () => {
+console.log(Files service running on port ${process.env.PORT});
+sendLog('Files service started');
+});
+}).catch(console.error);
